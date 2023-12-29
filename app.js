1d32b59a44dbcc5cfc6027c06e55ccf0ee4244df
Elena Vasileva <milena-iron@yandex.ru>
2023-12-29 16:38:25 +0700

    fix: исправить ошибки при тесте на гитхабе(6)

--- app.js
+++ app.js
@@ -26,4 +26,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // для приёма ве�
 app.use("/", require("./routes/users"));
 app.use("/", require("./routes/cards"));
 
+app.all("*", (req, res) => {
+  res.status(404).send({ message: "Страница не найдена" });
+});
+
 app.listen(PORT);
