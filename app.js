bd02dac1df96c05359300ba9bc22e10ca75d50f3
Elena Vasileva <milena-iron@yandex.ru>
2024-01-23 02:30:38 +0700

    fix: изменение валидации аватара

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().email().required(), // .pattern(URL) ВСТАВИТЬ???????????
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
@@ -55,8 +55,10 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string(), // .pattern(URL) ВСТАВИТЬ???????????????????????????
-      email: Joi.string().email().required(), // .pattern(URL) ВСТАВИТЬ???????
+      avatar: Joi.string().pattern(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/
+      ),
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
   }),
--- models/user.js
+++ models/user.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
       validate: {
         validator: (avatar) => {
           // регулярное выражение
-          /https?:\/\/(www\.)?[a-zA-Z0-9-@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([a-zA-Z0-9()-@:%_+.~#?&//=]*)/.test(
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/.test(
             avatar
           );
         },
